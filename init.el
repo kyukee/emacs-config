3f8f51cd3d12fc7dc062f78fd623351af99c4de1
Iulian Puscasu <iulian.puscasu.mail@gmail.com>
2021-02-18 01:07:36 +0000

    update list of fixes and work to be done

--- init.el
+++ init.el
@@ -5,10 +5,9 @@
 
 ;;; Code:
 
+;; If config.el exists then load it, otherwise tangle config.org
 (if (file-exists-p (expand-file-name "config.el" user-emacs-directory))
     (load-file (expand-file-name "config.el" user-emacs-directory))
   (org-babel-load-file (expand-file-name "config.org" user-emacs-directory)))
-
-(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 
 ;;; init.el ends here
