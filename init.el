824147b2f9cfb815876b203ae886f73220ef6ed4
Iulian Puscasu <iulian.puscasu.mail@gmail.com>
2021-08-10 18:36:15 +0100

    patch org-babel-tangle

--- init.el
+++ init.el
@@ -8,9 +8,14 @@
 (defvar *config-file* (expand-file-name "README.org" user-emacs-directory)
   "The configuration file.")
 
-;; If config.el exists then load it, otherwise tangle README.org
-(if (file-exists-p (expand-file-name "config.el" user-emacs-directory))
-    (load-file (expand-file-name "config.el" user-emacs-directory))
+(defvar *config-file-tangled* (expand-file-name "config.el" user-emacs-directory)
+  "The configuration file, after being tangled.")
+
+(load-file (expand-file-name "resources/tangle-patch.el" user-emacs-directory))
+
+;; If tangled elisp exists then load it, otherwise tangle the Org file and load it
+(if (file-exists-p *config-file-tangled*)
+    (load-file *config-file-tangled*)
   (org-babel-load-file *config-file*))
 
 ;;; init.el ends here
---
+++ resources/tangle-patch.el
@@ -0,0 +1,38 @@
+(require 'ob-tangle)
+(load-file "/home/kyukee/.emacs.d/straight/repos/org/lisp/ob-tangle.el")
+
+(defun *org-babel-tangle-collect-blocks (&optional lang-re tangle-file)
+  "Like `org-babel-tangle-collect-blocks', but will ignore blocks that are in trees with the :notangle: tag."
+  (let ((counter 0) last-heading-pos blocks)
+    (org-babel-map-src-blocks (buffer-file-name)
+      (let ((current-heading-pos
+	           (org-with-wide-buffer
+	            (org-with-limited-levels (outline-previous-heading)))))
+	      (if (eq last-heading-pos current-heading-pos) (cl-incf counter)
+	        (setq counter 1)
+	        (setq last-heading-pos current-heading-pos)))
+      (unless (or (org-in-commented-heading-p)
+		              (org-in-archived-heading-p))
+	      (let* ((tags (org-get-tags-at))
+               (info (org-babel-get-src-block-info 'light))
+	             (src-lang (nth 0 info))
+	             (src-tfile (cdr (assq :tangle (nth 2 info)))))
+	        (unless (or (member "notangle" tags)
+                      (string= src-tfile "no")
+		                  (and tangle-file (not (equal tangle-file src-tfile)))
+		                  (and lang-re (not (string-match-p lang-re src-lang))))
+	          ;; Add the spec for this block to blocks under its tangled
+	          ;; file name.
+	          (let* ((block (org-babel-tangle-single-block counter))
+                   (src-tfile (cdr (assq :tangle (nth 4 block))))
+		               (file-name (org-babel-effective-tangled-filename
+                               (nth 1 block) src-lang src-tfile))
+		               (by-fn (assoc file-name blocks)))
+	            (if by-fn (setcdr by-fn (cons (cons src-lang block) (cdr by-fn)))
+		            (push (cons file-name (list (cons src-lang block))) blocks)))))))
+    ;; Ensure blocks are in the correct order.
+    (mapcar (lambda (b) (cons (car b) (nreverse (cdr b))))
+	          (nreverse blocks))))
+
+(advice-add 'org-babel-tangle-collect-blocks :override
+             #'*org-babel-tangle-collect-blocks)
